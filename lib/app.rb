283efa7e8fb41e8af7e4152a6ef193c4c1180548
Madelene Campos <madelenecampos@gmail.com>
2016-08-14 00:15:29 -0400

    Able to create new issue using Mongoid

--- lib/app.rb
+++ lib/app.rb
@@ -1,5 +1,9 @@
-require "models/issue"
+require_relative "models/issue"
+
 class App < Sinatra::Base
+  enable :sessions
+  register Sinatra::Flash
+
   get "/" do
     redirect "/issues"
   end
@@ -7,5 +11,17 @@ class App < Sinatra::Base
   get "/issues" do
     @issues = Issue.all
     haml :"issues/index"
+  end
+
+  get "/issues/new" do
+    @issue = Issue.new
+    haml :"issues/new"
+  end
+
+  post "/issues" do
+    @issue = Issue.new params[:issue]
+    if @issue.save
+      redirect "/"
+    end
   end
 end
